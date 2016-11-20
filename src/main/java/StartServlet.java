fc6c1c09b2ce2287a9db45411d5d96b0cc07470e
marcin <marcin@mar.pl>
2016-11-20 12:37:58 +0100

    fixed .idea gitignore

---
+++ src/main/java/StartServlet.java
@@ -0,0 +1,24 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Created by marcin.kowalski on 20/11/16.
+ */
+
+public class StartServlet extends HttpServlet {
+
+	@Override protected void doGet(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws ServletException, IOException {
+
+		PrintWriter out = httpServletResponse.getWriter();
+		out.println("START");
+		httpServletResponse.sendRedirect("/hiddenservlet/results.do");
+
+
+	}
+}
